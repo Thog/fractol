45c11bc62fff80c4677c63528789f8684a42523d
Thog <thog92@hotmail.fr>
2016-06-29 11:21:45 +0200

    Add a null check for display!

--- src/main.c
+++ src/main.c
@@ -24,9 +24,9 @@ static void	*init_fractal(char *type)
 int			init_display(t_env *env)
 {
 	if (!(env->mlx = mlx_init()) ||
-			!(env->win = mlx_new_window(env->mlx, WIDTH, HEIGHT, "fractol")))
-		return (1);
-	env->render = new_img(env->mlx, WIDTH, HEIGHT);
+			!(env->win = mlx_new_window(env->mlx, WIDTH, HEIGHT, "fractol"))
+			|| !(env->render = new_img(env->mlx, WIDTH, HEIGHT)))
+		return (ft_error_retint("Display cannot be initialized!\n", 1));
 	ft_bzero(env->render->data, env->render->line_size * HEIGHT);
 	mlx_key_hook(env->win, key_hook, env);
 	mlx_expose_hook(env->win, expose_hook, env);
@@ -68,9 +68,9 @@ int			main(int ac, char **av)
 {
 	t_env	*env;
 
-	if (!(env = (t_env*)ft_memalloc(sizeof(t_env))) || init_data(env, ac, av))
+	if (!(env = (t_env*)ft_memalloc(sizeof(t_env))) || init_data(env, ac, av) ||
+		init_display(env))
 		return (destroy_env(env, 1));
-	init_display(env);
 	env->update = 1;
 	mlx_loop(env->mlx);
 	return (destroy_env(env, 0));
